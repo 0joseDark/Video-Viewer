420c5492bba8957dae7c0eff1166f02d35b05379
jose <162716366+0joseDark@users.noreply.github.com>
2025-08-08 23:57:08 +0100

    Update Video-Viewer.py

--- Video-Viewer.py
+++ Video-Viewer.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Editor / Exibidor de Vídeo simples usando PyQt5
-Funcionalidades:
- - Menu: Ficheiro -> Abrir, Sair
- - Botões: Ligar (Play), Pausa (Pause), Desligar (Stop)
- - Slider de progresso, rótulo com tempo, controlo de volume
-
-Instruções:
- 1) Instalar dependências: pip install PyQt5
- 2) Executar: python editor_exibidor_video_qt.py
-
-Compatível com Windows, Linux e macOS (desde que PyQt5 esteja instalado).
-"""
-
 import sys
 from pathlib import Path
 from PyQt5.QtWidgets import (
